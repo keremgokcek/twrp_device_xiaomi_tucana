902b28b7a9e2ef9c47ba557fa2bef9f0a27a1f60
Darth9 <reyprimerjefe@gmail.com>
2022-08-02 20:30:28 +0200

    tucana: Update the inheritances for 12.1

--- twrp_tucana.mk
+++ twrp_tucana.mk
@@ -18,7 +18,8 @@
 PRODUCT_RELEASE_NAME := tucana
 DEVICE_PATH := device/xiaomi/tucana
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
